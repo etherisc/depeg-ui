3f1955f9c334c9c06963f47f7e2889c96dd92b20
Marc Doerflinger <marc@doerf.li>
2022-11-07 20:36:41 +0000

    footer formatting

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -14,6 +14,8 @@ export default function Footer() {
             <Container maxWidth="xl" sx={{
                 position: "fixed",
                 bottom: 0,
+                left: 0,
+                right: 0,
                 bgcolor: "#f9f9f9",
             }}>
                 <Box sx={{ py: 1 }} display="flex" flexWrap="wrap" justifyContent="right" >
