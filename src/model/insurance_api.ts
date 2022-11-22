6d91599319a8c86070890aec6d43ff1c8431ac59
Marc Doerflinger <marc@doerf.li>
2022-11-22 09:36:25 +0000

    refactor InsuranceApiSmartContract into class

--- src/model/insurance_api.ts
+++ src/model/insurance_api.ts
@@ -2,7 +2,7 @@ import { ethers, Signer } from "ethers";
 import { SnackbarMessage, OptionsObject, SnackbarKey } from "notistack";
 import { BundleData } from "../application/insurance/bundle_data";
 import { insuranceApiMock } from "../application/insurance/insurance_api_mock";
-import { insuranceApiSmartContract } from "../application/insurance/insurance_api_smart_contract";
+import { InsuranceApiSmartContract } from "../application/insurance/insurance_api_smart_contract";
 import { PolicyRowView } from "./policy";
 
 export interface InsuranceApi {
@@ -90,9 +90,9 @@ export function getInsuranceApi(
     } else {
         console.log("Using smart contract", depegProductContractAddress);
         if (signer === undefined || provider === undefined) {
-            return insuranceApiSmartContract(new ethers.VoidSigner(depegProductContractAddress, provider), depegProductContractAddress, enqueueSnackbar, t);
+            return new InsuranceApiSmartContract(new ethers.VoidSigner(depegProductContractAddress, provider), depegProductContractAddress, t);
         } else {
-            return insuranceApiSmartContract(signer, depegProductContractAddress, enqueueSnackbar, t);
+            return new InsuranceApiSmartContract(signer, depegProductContractAddress, t);
         }
     }
 }
