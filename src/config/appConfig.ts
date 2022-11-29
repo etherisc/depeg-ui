38df3b75e76612710e3c894dd0614c369fdaeb96
Marc Doerflinger <marc@doerf.li>
2022-11-29 09:39:19 +0000

    chain url via env var

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -3,9 +3,15 @@
 
 // export const fujiEthProvider = new StaticJsonRpcProvider('https://api.avax-test.network/ext/bc/C/rpc');
 
+const chainId = process.env.NEXT_PUBLIC_CHAIN_ID;
+const chainUrl = process.env.NEXT_PUBLIC_CHAIN_URL;
+
+console.log("wallet connect config", "chainid", chainId, "chainUrl", chainUrl);
+
 export const walletConnectConfig = {
+    projectId: '6cf24be37dc19d58bc113806ab03aded',
     rpc: {
         "43113": "https://api.avax-test.network/ext/bc/C/rpc", // fuji
-        "1234": " http://127.0.0.1:7545", // ganache localhost
+        chainId: chainUrl, // main chain connection
     },
 };
