27649c60ba22e238220c9ca06c1a44faf7f187fb
Marc Doerflinger <marc@doerf.li>
2022-12-05 12:29:35 +0000

    remove entrypoint file

--- bin/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# dump all env variables for next.js to .env.production file
-env | grep '^NEXT_PUBLIC' > .env.production
-echo '.env.production contents'
-cat .env.production
-
-# now start next.js server
-node server.js
