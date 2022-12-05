0ee9fecfbceff2109a2bfa676b7a491dc91d6d9a
Marc Doerflinger <marc@doerf.li>
2022-12-05 16:19:21 +0000

    fix faucet (closes #61)

--- src/components/shared/faucet/faucet.tsx
+++ src/components/shared/faucet/faucet.tsx
@@ -7,10 +7,11 @@ import { transferAmount } from "../../../backend/erc20";
 import { DepegProduct__factory } from "../../../contracts/depeg-contracts";
 import { useSnackbar } from 'notistack';
 import { useTranslation } from "next-i18next";
+import { parseEther } from "ethers/lib/utils";
 
 export default function Faucet() {
     const appContext = useContext(AppContext);
-    const { enqueueSnackbar } = useSnackbar();
+    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
     const { t } = useTranslation('common');
 
     const [ address, setAddress ] = useState<string|undefined>(undefined);
@@ -48,17 +49,27 @@ export default function Faucet() {
         // TODO: init coin source account (and do not allow undefined)
         const coinSourceSigner: Signer | undefined = ethers.Wallet.fromMnemonic(process.env.NEXT_PUBLIC_FAUCET_MNEMONIC ?? "").connect(appContext.data.provider!);
 
-        // transfer some eth to pay for trx
-        let ethTx = {
-            to: address!,
-            value: 10000000000000000000 // 10 ETH
-        };
-        const tx = await coinSourceSigner!.sendTransaction(ethTx);
-        await tx.wait();
+        const snackbarId = enqueueSnackbar(t('wait_for_coins'),  { persist: true, variant: 'info' });
 
-        // transfer some testcoin
-        const tokenAddress = await getTokenAddress();
-        transferAmount(address!, 1000000000000, tokenAddress, coinSourceSigner!);  // 1'000'000 DEP
+        if (process.env.NEXT_PUBLIC_FAUCET_SEND_ETHERS === "true") {
+        // transfer some eth to pay for trx
+            let ethTx = {
+                to: address!,
+                gasLimit: 50000,
+                value: parseEther("10.0") // 10 ETH
+            };
+            console.log("sending eth to", ethTx.to);
+            const tx = await coinSourceSigner!.sendTransaction(ethTx);
+            await tx.wait();
+        }
+
+        if (process.env.NEXT_PUBLIC_FAUCET_SEND_TESTCOIN === "true") {
+            // transfer some testcoin
+            const tokenAddress = await getTokenAddress();
+            transferAmount(address!, 1000000000000, tokenAddress, coinSourceSigner!);  // 1'000'000 DEP
+        }
+        
+        closeSnackbar(snackbarId);
         enqueueSnackbar(t('coins_sent'),  { autoHideDuration: 3000, variant: 'success' });
     }
     
