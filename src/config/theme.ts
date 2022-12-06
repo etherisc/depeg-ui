2b85c086437191cd0823a29fe2e3e1355f6f0b9a
Marc Doerflinger <marc@doerf.li>
2022-12-06 14:30:57 +0000

    adapt colors (#76)

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -12,20 +12,20 @@ import red from '@mui/material/colors/red';
 export const etheriscTheme = createTheme({
     palette: {
         background: {
-            default: "#EBF0FC",
+            default: 'hsl(222,74%,96%)'
         },
         primary: {
             // light: will be calculated from palette.primary.main,
-            main: '#5081F4',
+            main: '#2B66EE',
             // dark: will be calculated from palette.primary.main,
             // contrastText: will be calculated to contrast with palette.primary.main
         },
         secondary: {
             // light: will be calculated from palette.secondary.main,
-            main: '#F8B83A', // yello
-            // main: '#41D895', // green
+            main: "#9577F7",
             // dark: will be calculated from palette.secondary.main,
             // contrastText: will be calculated to contrast with palette.secondary.main
+            contrastText: "#fff"
         },
     },
 });
