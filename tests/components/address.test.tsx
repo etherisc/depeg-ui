0bd24afea8be4c7f0bfb00b91bf33ed03654ffab
Marc Doerflinger <marc@doerf.li>
2023-01-20 20:01:49 +0000

    i18n workaround

--- tests/components/address.test.tsx
+++ tests/components/address.test.tsx
@@ -12,6 +12,19 @@ Object.assign(navigator, {
     },
 });
 
+jest.mock('react-i18next', () => ({
+    ...jest.requireActual('react-i18next'),
+    // this mock makes sure any components using the translate hook can use it without a warning being shown
+    useTranslation: () => {
+        return {
+            t: (str: string) => str,
+            i18n: {
+                changeLanguage: () => new Promise(() => {}),
+            },
+        };
+    },
+}));    
+
 describe('Address', () => {
     it('renders shortened address', () => {
         render(
