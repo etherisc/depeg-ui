5ed956fb127c44f0513c83193f9d77e8e540bc91
Marc Doerflinger <marc@doerf.li>
2023-01-23 13:10:20 +0000

    fix rendering issue

--- src/components/with_tooltip.tsx
+++ src/components/with_tooltip.tsx
@@ -4,7 +4,7 @@ export default function WithTooltip(props: any) {
     const { tooltipText, children, typographyVariant } = props;
     return (<>
         <Tooltip title={tooltipText} enterDelay={700} leaveDelay={200} sx={{ display: 'inline-block'}}>
-            <Typography variant={ typographyVariant || 'body2' }>
+            <Typography variant={ typographyVariant || 'body2' } component="span">
                 {children}
             </Typography>
         </Tooltip>
