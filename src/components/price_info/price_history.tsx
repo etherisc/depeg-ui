461e56f492d9dbf457668230761e34be737e48ff
Marc Doerflinger <marc@doerf.li>
2023-01-26 15:16:22 +0000

    trigger and recovery line (#115)

--- src/components/price_info/price_history.tsx
+++ src/components/price_info/price_history.tsx
@@ -1,10 +1,14 @@
 import { formatUnits } from "ethers/lib/utils";
 import { Line } from "react-chartjs-2";
 import { CategoryScale, Chart, LinearScale, PointElement, LineElement, Colors, TimeScale, Tooltip } from 'chart.js'; 
-import { LinearProgress } from "@mui/material";
+import { LinearProgress, useTheme } from "@mui/material";
 import FakeData from "./fake_data";
 import 'chartjs-adapter-moment';
 import { useTranslation } from "next-i18next";
+import Color from "color";
+
+const TRIGGER_PRICE = 0.995;
+const RECOVERY_PRICE = 0.999;
 
 
 interface PriceHistoryProps {
@@ -19,6 +23,7 @@ Chart.register(CategoryScale, LinearScale, PointElement, LineElement, Colors, Ti
 
 export default function PriceHistory(props: PriceHistoryProps) {
     const { t } = useTranslation(['price', 'common']);
+    const theme = useTheme();
 
     let labels = [] as string[];
     let dataset = [] as any;
@@ -28,7 +33,7 @@ export default function PriceHistory(props: PriceHistoryProps) {
             (price: PriceInfo) => {
                 // const ts = moment.utc(price.timestamp * 1000).format("MM-DD HH:mm");
                 // const ts = price.timestamp;
-                return { 
+                return {
                     x: price.timestamp * 1000, 
                     y: parseFloat(formatUnits(price.price, props.decimals)) 
                 }
@@ -38,10 +43,29 @@ export default function PriceHistory(props: PriceHistoryProps) {
 
     const data = {
         labels,
-        datasets: [{
-            // label: `{props.symbol} price history`,
-            data: dataset,
-        }]
+        datasets: [
+            {
+                label: `{props.symbol} price history`,
+                data: dataset,
+                borderColor: theme.palette.primary.light,
+            },
+            {
+                label: "Trigger",
+                data: props.prices.length > 0 ? [
+                    { x: props.prices[0].timestamp * 1000, y: TRIGGER_PRICE },
+                    { x: props.prices[props.prices.length - 1].timestamp * 1000, y: TRIGGER_PRICE },
+                ] : [],
+                borderColor: Color(theme.palette.error.light).fade(0.7).hexa(),
+            },
+            {
+                label: "Recovery",
+                data: props.prices.length > 0 ? [
+                    { x: props.prices[0].timestamp * 1000, y: RECOVERY_PRICE },
+                    { x: props.prices[props.prices.length - 1].timestamp * 1000, y: RECOVERY_PRICE },
+                ] : [],
+                borderColor: Color(theme.palette.success.light).fade(0.7).hexa(),
+            },
+        ]
     };
 
     const options = {
@@ -62,8 +86,8 @@ export default function PriceHistory(props: PriceHistoryProps) {
                     display: true,
                     text: t('chart.y.title', { symbol: props.symbol })
                 },
-                suggestedMin: 0.98,
-                suggestedMax: 1.02,
+                suggestedMin: 0.975,
+                suggestedMax: 1.015,
             }
         },
         elements: {
