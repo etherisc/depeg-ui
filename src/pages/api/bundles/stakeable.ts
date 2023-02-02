91d0b119ce1a9f054a67e8a9531f6c42587a12c7
Marc Doerflinger <marc@doerf.li>
2023-02-02 15:54:20 +0000

    add api for stakeable bundles (#221)

---
+++ src/pages/api/bundles/stakeable.ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { BundleData } from "../../../backend/bundle_data"
+import { StaticJsonRpcProvider } from '@ethersproject/providers';
+import { BigNumber, ethers, Signer } from "ethers";
+import { DepegProduct, DepegProduct__factory, DepegRiskpool } from "../../../contracts/depeg-contracts";
+import { getDepegRiskpool, getInstanceService } from "../../../backend/gif_registry";
+import { IInstanceService } from "../../../contracts/gif-interface/IInstanceService";
+import { DepegRiskpoolApi } from "../../../backend/riskpool_api";
+import { redisClient } from "../../../utils/redis";
+
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Array<BundleData>>
+) {
+    console.log("getting all bundles from redis");
+    const bundlesjson = await redisClient.get("bundles");
+    
+    if (bundlesjson == null) {
+        res.status(500).json([]);
+        return;
+    }        
+
+    const bundles = JSON.parse(bundlesjson) as Array<BundleData>;
+
+    res.status(200).json(bundles.filter(bundle => {
+        const capacity = BigNumber.from(bundle.capacity);
+        // ignore bundles with no capacity
+        if (capacity.lte(0)) {
+            return false;
+        }
+        // ignore bundles with less capacity then min protected amount (inconsistent)
+        if (BigNumber.from(bundle.minSumInsured).gt(capacity)) {
+            return false;
+        }
+        const capitalSupport = bundle.capitalSupport;
+        if (capitalSupport !== undefined) {
+            // console.log("bundleid", bundle.id, "locked", bundle.locked, "capitalSupport", capitalSupport.toString());
+            // if supported capital is defined, then only bundles with locked capital less than the capital support are used
+            if (BigNumber.from(bundle.locked).gte(BigNumber.from(capitalSupport))) {
+                return false;
+            }
+            console.log("stakes available", bundle.id);
+        }
+        return true;
+    }));
+}
+
