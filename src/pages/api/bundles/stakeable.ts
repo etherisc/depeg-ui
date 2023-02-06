94b39216e9e6e1721b57c9e7a88d432036e7a5e2
Marc Doerflinger <marc@doerf.li>
2023-02-06 14:34:11 +0000

    update bundles (#221)

--- src/pages/api/bundles/stakeable.ts
+++ src/pages/api/bundles/stakeable.ts
@@ -28,27 +28,31 @@ export default async function handler(
     res.status(200).json(bundles.filter(bundle => {
         // ignore expired bundles
         if (lastBlockTimestamp > (bundle.createdAt + parseInt(bundle.lifetime))) {
+            console.log("bundle expired", bundle.id);
             return false;
         }
 
         const capacity = BigNumber.from(bundle.capacity);
         // ignore bundles with no capacity
         if (capacity.lte(0)) {
+            console.log("bundle no capacity", bundle.id);
             return false;
         }
         // ignore bundles with less capacity then min protected amount (inconsistent)
         if (BigNumber.from(bundle.minSumInsured).gt(capacity)) {
+            console.log("bundle minSumInsured greater than capacity", bundle.id);
             return false;
         }
         const capitalSupport = bundle.capitalSupport;
         if (capitalSupport !== undefined) {
-            // console.log("bundleid", bundle.id, "locked", bundle.locked, "capitalSupport", capitalSupport.toString());
+            console.log("bundleid", bundle.id, "locked", bundle.locked, "capitalSupport", capitalSupport.toString());
             // if supported capital is defined, then only bundles with locked capital less than the capital support are used
             if (BigNumber.from(bundle.locked).gte(BigNumber.from(capitalSupport))) {
+                console.log("bundle locked capital greater than capital support", bundle.id);
                 return false;
             }
-            console.log("stakes available", bundle.id);
         }
+        console.log("bundle stakeable", bundle.id);
         return true;
     }));
 }
