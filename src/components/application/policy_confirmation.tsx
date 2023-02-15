8d3fadd7a905f2cef29cc156606ace127c351cc2
Marc Doerflinger <marc@doerf.li>
2023-02-15 15:34:46 +0000

    update text

--- src/components/application/policy_confirmation.tsx
+++ src/components/application/policy_confirmation.tsx
@@ -8,6 +8,7 @@ import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import { useSnackbar } from "notistack";
 import { formatAddress } from "../../utils/address";
+import { formatCurrencyBN } from "../../utils/numbers";
 
 interface PolicyConfirmationProps {
     processId: string;
@@ -35,72 +36,21 @@ export default function PolicyConfirmation(props: PolicyConfirmationProps) {
             <Grid item xs={12}>
                 <Alert severity="success" variant="filled" sx={{ mb: 2 }}>
                     <AlertTitle>{t('confirmation.alert.title')}</AlertTitle>
-                    {t('confirmation.alert.text')}
+                    <Typography variant="body2">
+                    {t('confirmation.alert.text', {
+                        walletAddress: formatAddress(props.wallet),
+                        amount: formatCurrencyBN(props.amount, props.currencyDecimals),
+                        currency: props.currency,
+                        endDate: moment().add(props.coverageDurationSeconds, 's').format("DD/MM/YYYY"),
+                        policyId: props.processId
+                    })}
+                    </Typography>
                 </Alert>
             </Grid>
             <Grid item xs={12}>
-                <Card sx={{ minWidth: 275 }}>
-                    <CardContent>
-                        <Typography variant="h5">
-                            {t('confirmation.title')}
-                        </Typography>
-                        <Grid container spacing={1} sx={{ mt: 1}}>
-                            <Grid item xs={3}>
-                                <Typography variant="body1">
-                                    {t('confirmation.id')}:
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={9}>
-                                <Typography variant="body1">
-                                    {props.processId}
-                                    &nbsp;
-                                    <Typography component="span" color={theme.palette.secondary.main}>
-                                        <FontAwesomeIcon icon={faCopy} className="fa cursor-pointer" onClick={() => copyAddressToClipboard(props.processId)} />
-                                    </Typography>
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={3}>
-                                <Typography variant="body1">
-                                    {t('confirmation.wallet')}:
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={9}>
-                                <Typography variant="body1">
-                                    {props.wallet}
-                                    &nbsp;
-                                    <Typography component="span" color={theme.palette.secondary.main}>
-                                        <FontAwesomeIcon icon={faCopy} className="fa cursor-pointer" onClick={() => copyAddressToClipboard(props.wallet)} />
-                                    </Typography>
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={3}>
-                                <Typography variant="body1">
-                                {t('confirmation.amount')}:
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={9}>
-                                <Typography variant="body1">
-                                    {props.currency} {formatUnits(props.amount, props.currencyDecimals)}
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={3}>
-                                <Typography variant="body1">
-                                {t('confirmation.end_date')}:
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={9}>
-                                <Typography variant="body1">
-                                    {moment().add(props.coverageDurationSeconds, 's').format("YYYY-MM-DD")}
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small" variant="text" onClick={() => router.push("/policies")}>
-                            {t('confirmation.continue_link')}
-                        </Button>
-                    </CardActions>
-                </Card>
+                <Button size="small" variant="text" onClick={() => router.push("/policies")}>
+                    {t('confirmation.continue_link')}
+                </Button>
             </Grid>
         </Grid>
     </>);
