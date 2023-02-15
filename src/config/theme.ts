7156941ffb270c12301d45bb5b35e459146734fe
Marc Doerflinger <marc@doerf.li>
2023-02-15 09:26:41 +0000

    theme color change

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,5 +1,4 @@
 import { createTheme } from '@mui/material/styles';
-import red from '@mui/material/colors/red';
 
 // etherisc main colors
 // red: EE4620
@@ -22,11 +21,12 @@ export const etheriscTheme = createTheme({
             // contrastText: will be calculated to contrast with palette.primary.main
         },
         secondary: {
-            // light: will be calculated from palette.secondary.main,
-            main: "#9577F7",
+            // main: "#9577F7", // purple
+            main: "#F8B83A", // yellow
             // dark: will be calculated from palette.secondary.main,
             // contrastText: will be calculated to contrast with palette.secondary.main
-            contrastText: "#fff"
+            // contrastText: "#fff" // white
+            contrastText: "#000000DE" // black
         },
     },
     components: {
