be327ab6bb08d0b9022e2573ffc8aa67ab3287fb
Marc Doerflinger <marc@doerf.li>
2023-02-15 13:57:48 +0000

    add back arrow (#40)

--- src/components/bundles/show_bundle.tsx
+++ src/components/bundles/show_bundle.tsx
@@ -1,7 +1,10 @@
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Grid, Typography } from "@mui/material";
 import { useTranslation } from "next-i18next";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { BackendApi } from "../../backend/backend_api";
+import { showBundle } from "../../redux/slices/bundles";
 import { RootState } from "../../redux/store";
 import BundleDetails from "./bundle_details";
 
@@ -12,8 +15,20 @@ interface ShowBundleProps {
 export default function ShowBundle(props: ShowBundleProps) {
     const { t } = useTranslation('bundles');
     const bundle = useSelector((state: RootState) => state.bundles.showBundle);
+    const dispatch = useDispatch();
+    
     return (<>
-        <Typography variant="h5" mb={2}>{t('title_show_bundle')} <b>{bundle?.name}</b> (#{bundle?.id})</Typography>
+        <Typography variant="h5" mb={2}>
+            <Typography variant="h6" mb={2} component="span">
+                <FontAwesomeIcon icon={faArrowLeft} style={{ cursor: 'pointer' }} onClick={() => dispatch(showBundle(undefined))} />
+            </Typography>
+            &nbsp;
+            {t('title_show_bundle')}
+            &nbsp;
+            <b>{bundle?.name}</b>
+            &nbsp;
+            (#{bundle?.id})
+        </Typography>
 
         <Grid container spacing={2} sx={{ mt: 2 }}>
             <Grid item xs={12} md={6}>
