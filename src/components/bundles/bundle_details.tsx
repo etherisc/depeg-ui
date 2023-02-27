5f8846f9ae0509da2392612e6fd3fc19f8caa7cf
Marc Doerflinger <marc@doerf.li>
2023-02-27 19:36:40 +0000

    add tooltips (#40)

--- src/components/bundles/bundle_details.tsx
+++ src/components/bundles/bundle_details.tsx
@@ -5,6 +5,7 @@ import { BundleData } from "../../backend/bundle_data";
 import { formatAddress } from "../../utils/address";
 import { formatDateTimeUtc } from "../../utils/date";
 import { formatCurrencyBN } from "../../utils/numbers";
+import WithTooltip from "../with_tooltip";
 
 interface BundleDetailsProps {
     bundle: BundleData;
@@ -32,12 +33,11 @@ export default function BundleDetails(props: BundleDetailsProps) {
     return (<>
         <Grid container spacing={1} data-testid="bundle-details">
             <NameValue name={t('state')} value={t('bundle_state_' + state, { ns: 'common'})}/>
-            {/* TODO: MZ difference between balance and capacity? -> make note in ui */}
-            <NameValue name={t('balance')} value={symbol + " " + formatCurrencyBN(balance, props.decimals)}/>
-            <NameValue name={t('capacity')} value={symbol + " " + formatCurrencyBN(capacity, props.decimals)}/>
-            <NameValue name={t('locked')} value={symbol + " " + formatCurrencyBN(locked, props.decimals)}/>
-            <NameValue name={t('supported_capital')} value={symbol + " " + formatCurrencyBN(capitalSupport, props.decimals)}/>
-            <NameValue name={t('supported_capital_remaining')} value={symbol + " " + formatCurrencyBN(capitalSupportRemaining, props.decimals)}/>
+            <NameValue name={t('balance')} value={symbol + " " + formatCurrencyBN(balance, props.decimals)} tooltip={t('balance_tooltip')} />
+            <NameValue name={t('capacity')} value={symbol + " " + formatCurrencyBN(capacity, props.decimals)} tooltip={t('capacity_tooltip')} />
+            <NameValue name={t('locked')} value={symbol + " " + formatCurrencyBN(locked, props.decimals)} tooltip={t('locked_tooltip')} />
+            <NameValue name={t('supported_capital')} value={symbol + " " + formatCurrencyBN(capitalSupport, props.decimals)} tooltip={t('supported_capital_tooltip')} />
+            <NameValue name={t('supported_capital_remaining')} value={symbol + " " + formatCurrencyBN(capitalSupportRemaining, props.decimals)} tooltip={t('supported_capital_remaining_tooltip')} />
             <NameValue name={t('min_max_sum_insured')} value={symbol + " " + formatCurrencyBN(minSumInsured, props.decimals) + " / " + formatCurrencyBN(maxSumInsured, props.decimals)}/>
             <NameValue name={t('min_max_duration')} value={minDuration + " / " + " " + maxDuration + " " + t('days')}/>
             <NameValue name={t('apr')} value={props.bundle.apr + " %"}/>
@@ -49,13 +49,19 @@ export default function BundleDetails(props: BundleDetailsProps) {
     </>);
 }
 
-function NameValue(props: { name: string, value: string }) {
+function NameValue(props: { name: string, value: string, tooltip?: string }) {
+    let value = (<>{props.value}</>);
+    if (props.tooltip) {
+        value = (<WithTooltip tooltipText={props.tooltip} typographyVariant="body1">
+            {value}
+        </WithTooltip>);
+    }
     return (<>
         <Grid item xs={12} md={5}>
             {props.name}
         </Grid>
         <Grid item xs={12} md={7}>
-            {props.value}
+            {value}
         </Grid>
     </>);
 }
