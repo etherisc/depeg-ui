525fb967dc00ee3e44115cc939d8fa8fd57aefdc
Marc Doerflinger <marc@doerf.li>
2023-03-13 09:17:40 +0000

    show bundle actions as disabled instead of hiding (closes #337)

--- src/components/show_bundle/bundle_actions.tsx
+++ src/components/show_bundle/bundle_actions.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid } from "@mui/material";
+import { Alert, Button, Grid } from "@mui/material";
 import { useTranslation } from "next-i18next";
 import { BundleData } from "../../backend/bundle_data";
 import { RootState } from "../../redux/store";
@@ -31,8 +31,13 @@ export default function BundleActions(props: BundleActionsProps) {
     //     Burned
     // }
 
-    const state = props.bundle.state;
     const isOwner = props.connectedWallet !== undefined && props.connectedWallet === props.bundle.owner;
+    
+    if (!isOwner) {
+        return (<Alert severity="info">{t('alert.actions_only_owner')}</Alert>);
+    }
+
+    const state = props.bundle.state;
     const isLockAllowed = isOwner && (state === 0);
     const isUnlockAllowed = isOwner && (state === 1) && (props.activeBundles < props.maxActiveBundles);
     const isFundAllowed = isOwner && (state === 0 || state === 1);
@@ -66,48 +71,54 @@ export default function BundleActions(props: BundleActionsProps) {
 
     
     return (<Grid container spacing={2} data-testid="bundle-actions">
-        {isFundAllowed && <Grid item xs={12}>
+        <Grid item xs={12}>
             <Button 
                 onClick={fund}
                 variant="contained" 
                 sx={{ minWidth: '12rem' }}
+                disabled={!isFundAllowed}
                 >{t('action.fund')}</Button>
-        </Grid>}
-        { isWithdrawAllowed && <Grid item xs={12}>
+        </Grid>
+        <Grid item xs={12}>
             <Button 
                 onClick={withdraw}
                 variant="contained" 
                 sx={{ minWidth: '12rem' }}
+                disabled={!isWithdrawAllowed}
                 >{t('action.withdraw')}</Button>
-        </Grid>}
-        { isLockAllowed && <Grid item xs={12}>
+        </Grid>
+        <Grid item xs={12}>
             <Button 
                 onClick={lock}
                 variant="contained" 
                 sx={{ minWidth: '12rem' }}
+                disabled={!isLockAllowed}
                 >{t('action.lock')}</Button>
-        </Grid>}
-        { isUnlockAllowed && <Grid item xs={12}>
+        </Grid>
+        <Grid item xs={12}>
             <Button 
                 onClick={unlock}
                 variant="contained" 
+                disabled={!isUnlockAllowed}
                 sx={{ minWidth: '12rem' }}
                 >{t('action.unlock')}</Button>
-        </Grid>}
-        { isCloseAllowed && <Grid item xs={12}>
+        </Grid>
+        <Grid item xs={12}>
             <Button 
                 onClick={close}
                 variant="contained" 
+                disabled={!isCloseAllowed}
                 sx={{ minWidth: '12rem' }}
                 >{t('action.close')}</Button>
-        </Grid>}
-        { isBurnAllowed && <Grid item xs={12}>
+        </Grid>
+        <Grid item xs={12}>
             <Button 
                 onClick={burn}
                 variant="contained" 
+                disabled={!isBurnAllowed}
                 sx={{ minWidth: '12rem' }}
                 >{t('action.burn')}</Button>
-        </Grid>}
+        </Grid>
     </Grid>);
 }
 
