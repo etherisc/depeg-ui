a8b3a0387ce7cada99da35090bceb2ad6a3a974e
Marc Doerflinger <marc@doerf.li>
2023-03-14 14:52:21 +0000

    check bundle cap during staking (closes #239)

--- tests/components/show_bundle/bundle_fund_form.test.tsx
+++ tests/components/show_bundle/bundle_fund_form.test.tsx
@@ -62,7 +62,8 @@ describe('When rendering the BundleFundForm', () => {
                 currency="USDT"
                 decimals={6}
                 maxInvestedAmount={BigNumber.from("100000")}
-                getRemainingCapacity={async () => 100000}
+                getBundleCapitalCap={async () => parseUnits("100000", 6)}
+                getRemainingRiskpoolCapacity={async () => 100000}
                 doFund={fundMock}
                 doCancel={async () => true}
                 />
@@ -126,7 +127,7 @@ describe('When rendering the BundleFundForm', () => {
         });
     })
 
-    it('the remaining riskpool capacity is considered', async () => {
+    it('the bundle capital capacity is considered', async () => {
         const bundle = {
             id: 42,
             riskpoolId: 13,
@@ -157,7 +158,8 @@ describe('When rendering the BundleFundForm', () => {
                 currency="USDT"
                 decimals={6}
                 maxInvestedAmount={BigNumber.from("100000")}
-                getRemainingCapacity={async () => 5000}
+                getRemainingRiskpoolCapacity={async () => 50000}
+                getBundleCapitalCap={async () => parseUnits("10000", 6)}
                 doFund={fundMock}
                 doCancel={async () => true}
                 />
@@ -180,7 +182,75 @@ describe('When rendering the BundleFundForm', () => {
         const amountInput = screen.getByTestId("amount").querySelector("input");
 
         await waitFor(async () => {
-            expect(amountInput).toHaveAttribute("value", "5000");
+            expect(amountInput).toHaveAttribute("value", "0");
+        });
+
+        act(() => {
+            fireEvent.change(amountInput!, { target: { value: "10001" } });
+        });
+        await waitFor(async () => {
+            const e = await screen.findByTestId("amount");
+            return expect(e.querySelector("p.MuiFormHelperText-root")).toHaveTextContent("error.field.max");
+        });
+
+    })
+
+    it('the bundle capital capacity as well as the remaining riskpool capacity is considered', async () => {
+        const bundle = {
+            id: 42,
+            riskpoolId: 13,
+            owner: "0x2CeC4C063Fef1074B0CD53022C3306A6FADb4729",
+            tokenId: 7,
+            name: "Happy Testing",
+            apr: 3.1415,
+            capital: parseUnits("100000", 6).toString(),
+            balance: parseUnits("100123", 6).toString(),
+            capacity: parseUnits("90000", 6).toString(),
+            locked: parseUnits("10000", 6).toString(),
+            capitalSupport: parseUnits("80000", 6).toString(),
+            minSumInsured: parseUnits("1123", 6).toString(),
+            maxSumInsured: parseUnits("10456", 6).toString(),
+            minDuration: 11 * 24 * 60 * 60,
+            maxDuration: 28 * 24 * 60 * 60,
+            createdAt: 1676541508,
+            lifetime: BigNumber.from(28 * 24 * 60 * 60).toString(),    
+            state: 1,
+            policies: 789,
+        } as BundleData;
+
+        const fundMock = jest.fn();
+        
+        renderWithProviders(
+            <BundleFundForm
+                bundle={bundle}
+                currency="USDT"
+                decimals={6}
+                maxInvestedAmount={BigNumber.from("100000")}
+                getRemainingRiskpoolCapacity={async () => 5000}
+                getBundleCapitalCap={async () => parseUnits("10000", 6)}
+                doFund={fundMock}
+                doCancel={async () => true}
+                />
+            ,
+            {
+                preloadedState: {
+                    chain: {
+                        chainId: "0x0",
+                        isConnected: true,
+                        isExpectedChain: true,
+                        provider: undefined,
+                        signer: undefined,
+                        blockNumber: 0,
+                        blockTime: 0,
+                    }
+                }
+            }
+        );
+
+        const amountInput = screen.getByTestId("amount").querySelector("input");
+
+        await waitFor(async () => {
+            expect(amountInput).toHaveAttribute("value", "0");
         });
 
         act(() => {
