246c462a4e1409278eebcef7e46fc7cd32d91021
Marc Doerflinger <marc@doerf.li>
2023-03-23 16:09:35 +0000

    add count and page (#338)

--- src/pages/api/prices/all.tsx
+++ src/pages/api/prices/all.tsx
@@ -8,10 +8,20 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Array<PriceData>>
 ) {
-    // TODO: add filters
     console.log("called /api/prices/all");
     const priceRepository = await getPriceRepository();
-    const prices = await priceRepository.search().sortBy('roundId', 'DESC').return.all();
+
+    let query = await priceRepository.search().sortBy('roundId', 'DESC')
+    let prices;
+
+    if (req.query.page !== undefined || req.query.count !== undefined) {
+        const count = parseInt(req.query.count as string ?? "0");
+        const offset = parseInt(req.query.page as string ?? "0") * count;
+        prices = await query.return.page(offset, count);
+    } else {
+        prices = await query.return.all();
+    }
+
     res.status(200).json(prices.map(price => { 
         return {
                 roundId: price.roundId, 
