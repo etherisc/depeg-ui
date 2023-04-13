93c53cb21a5dbb075874fe15cda4d8276321d880
Marc Doerflinger <marc@doerf.li>
2023-04-13 09:07:43 +0000

    add web3-onboard (#58)

--- src/components/account/login_walletconnect.tsx
+++ src/components/account/login_walletconnect.tsx
@@ -1,16 +1,19 @@
 // import WalletConnectProvider from "@walletconnect/web3-provider";import { ethers } from "ethers";
-import { walletConnectConfig } from "../../config/appConfig";
-import Button from '@mui/material/Button'
+import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useMediaQuery, useTheme } from "@mui/material";
+import Button from '@mui/material/Button';
 import { useTranslation } from "next-i18next";
 import { useSnackbar } from "notistack";
-import { useMediaQuery, useTheme } from "@mui/material";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import { useDispatch, useSelector } from "react-redux";
+import Onboard from '@web3-onboard/core'
+import injectedModule from '@web3-onboard/injected-wallets'
+import { ethers } from 'ethers'
+import { toHex } from "../../utils/numbers";
 import { connectChain } from "../../redux/slices/chain";
-import { getAndUpdateBlock, getChainState, removeSigner, setAccountRedux, updateSigner } from "../../utils/chain";
+import { getChainState, setAccountRedux } from "../../utils/chain";
 
-// TODO: remove this class
+// TODO: rename to web3-onboard
 export default function LoginWithWalletConnectButton(props: any) {
     const { closeDialog } = props;
 
@@ -25,6 +28,42 @@ export default function LoginWithWalletConnectButton(props: any) {
         console.log("wallet connect login");
         closeDialog();
 
+        const MAINNET_RPC_URL = 'http://localhost:7545'
+
+        const injected = injectedModule();
+
+        const onboard = Onboard({
+        wallets: [injected],
+        chains: [
+            {
+            id: toHex(1337),
+            token: 'ETH',
+            label: 'Ganache',
+            rpcUrl: MAINNET_RPC_URL
+            }
+        ]
+        });
+
+        const wallets = await onboard.connectWallet();
+
+        console.log(wallets)
+
+        if (wallets[0]) {
+            // create an ethers provider with the last connected wallet provider
+            // if using ethers v6 this is:
+            // ethersProvider = new ethers.BrowserProvider(wallet.provider, 'any')
+            const ethersProvider = new ethers.providers.Web3Provider(wallets[0].provider, 'any')
+
+            dispatch(connectChain(await getChainState(ethersProvider)));
+            setAccountRedux(ethersProvider.getSigner(), dispatch);
+        }
+        
+        // TODO: accounts changed
+        // TODO: chain changed
+        // TODO: block update
+        // TODO: fix ui to handle web3-onboard component
+
+        // TODO: remove this
         //  Create WalletConnect Provider
         // const wcProvider = new WalletConnectProvider(walletConnectConfig);
 
@@ -42,7 +81,6 @@ export default function LoginWithWalletConnectButton(props: any) {
         //     );
         // }
 
-        // // TODO: make this implementation more robust
         // wcProvider.on("accountsChanged", async (accounts: string[]) => {
         //     console.log("accountsChanged", accounts);
         //     await provider?.send("eth_requestAccounts", []);
