beccc49be9ae141640586e37bd43d19eb3f6357a
Marc Doerflinger <marc@doerf.li>
2023-05-15 08:37:50 +0000

    header link (#487)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,21 +1,20 @@
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Divider, Link, List, ListItem, ListItemButton, ListItemIcon, ListItemText, useTheme } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
-import Account from '../account/account';
-import Image from 'next/image';
-import { useTranslation } from 'next-i18next';
-import { HeaderLink } from './header_link';
-import { useRouter } from 'next/router';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import { useState } from 'react';
-import MenuIcon from '@mui/icons-material/Menu';
 import Drawer from '@mui/material/Drawer';
-import { Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { useTranslation } from 'next-i18next';
+import Image from 'next/image';
+import { useState } from 'react';
+import Account from '../account/account';
 import FaucetListItem from './faucet_list_item';
+import { HeaderLink } from './header_link';
 
 interface HeaderProps {
     items: Array<[string, string, () => void | null, IconDefinition]>;
@@ -24,7 +23,7 @@ interface HeaderProps {
 
 export default function Header(props: HeaderProps) {
     const { t } = useTranslation('common');
-    const router = useRouter();
+    const theme = useTheme();
 
     const [mobileOpen, setMobileOpen] = useState(false);
     const drawerWidth = 240;
@@ -91,7 +90,11 @@ export default function Header(props: HeaderProps) {
                             <MenuIcon />
                         </IconButton>
                         <Box sx={{ mt: 0.25, display: { xs: 'none', md: 'inherit'}}}>
-                            <Image src="/etherisc_logo_white.svg" alt="Etherisc logo" width={100} height={22}  />
+                            <Link href="https://www.etherisc.com" target="_blank" rel="noreferrer">
+                                <Typography sx={{lineHeight: '16px', color: theme.palette.text.primary}} >
+                                    <Image src="/etherisc_logo_white.svg" alt="Etherisc logo" width={100} height={22}  />
+                                </Typography>
+                            </Link>
                         </Box>
                         <Box sx={{ display: { xs: 'inherit', md: 'none'}}}>
                             <Image src="/etherisc_logo_bird_white.svg" alt="Etherisc logo" width={28} height={22} />
