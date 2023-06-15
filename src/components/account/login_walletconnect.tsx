f6fb225f08cbe98489c1b354116f1ee6f246e939
Marc Doerflinger <marc@doerf.li>
2023-06-15 09:18:15 +0000

    remove wc v1

--- src/components/account/login_walletconnect.tsx
+++ src/components/account/login_walletconnect.tsx
@@ -1,4 +1,4 @@
-import WalletConnectProvider from "@walletconnect/web3-provider";import { ethers } from "ethers";
+import { ethers } from "ethers";
 import { walletConnectConfig } from "../../config/appConfig";
 import Button from '@mui/material/Button'
 import { useTranslation } from "next-i18next";
@@ -27,48 +27,48 @@ export default function LoginWithWalletConnectButton(props: any) {
         closeDialog();
 
         //  Create WalletConnect Provider
-        const wcProvider = new WalletConnectProvider(walletConnectConfig);
+        // const wcProvider = new WalletConnectProvider(walletConnectConfig);
 
-        try {
-            //  Enable session (triggers QR Code modal)
-            await wcProvider.enable();
-        } catch (error) {
-            enqueueSnackbar(
-                t('error.wallet_connect_failed'),
-                { 
-                    variant: 'warning',
-                    autoHideDuration: 4000,
-                    preventDuplicate: true,
-                }
-            );
-        }
+        // try {
+        //     //  Enable session (triggers QR Code modal)
+        //     await wcProvider.enable();
+        // } catch (error) {
+        //     enqueueSnackbar(
+        //         t('error.wallet_connect_failed'),
+        //         { 
+        //             variant: 'warning',
+        //             autoHideDuration: 4000,
+        //             preventDuplicate: true,
+        //         }
+        //     );
+        // }
 
-        // TODO: make this implementation more robust
-        wcProvider.on("accountsChanged", async (accounts: string[]) => {
-            console.log("accountsChanged", accounts);
-            await provider?.send("eth_requestAccounts", []);
-            updateSigner(dispatch, provider);
-            location.reload();
-        });
-        wcProvider.on("chainChanged", (chainId: number) => {
-            console.log("chainChanged", chainId);
-            if (chainId != 43113) {
-                wcProvider.disconnect();
-                removeSigner(dispatch);
-            }
-            location.reload();
-        });
+        // // TODO: make this implementation more robust
+        // wcProvider.on("accountsChanged", async (accounts: string[]) => {
+        //     console.log("accountsChanged", accounts);
+        //     await provider?.send("eth_requestAccounts", []);
+        //     updateSigner(dispatch, provider);
+        //     location.reload();
+        // });
+        // wcProvider.on("chainChanged", (chainId: number) => {
+        //     console.log("chainChanged", chainId);
+        //     if (chainId != 43113) {
+        //         wcProvider.disconnect();
+        //         removeSigner(dispatch);
+        //     }
+        //     location.reload();
+        // });
 
-        // A Web3Provider wraps a standard Web3 provider, which is
-        // what MetaMask injects as window.ethereum into each page
-        const provider = new ethers.providers.Web3Provider(wcProvider);
-        dispatch(connectChain(await getChainState(provider)));
-        setAccountRedux(provider.getSigner(), dispatch);
-        store.dispatch(fetchBalances(provider.getSigner()));
+        // // A Web3Provider wraps a standard Web3 provider, which is
+        // // what MetaMask injects as window.ethereum into each page
+        // const provider = new ethers.providers.Web3Provider(wcProvider);
+        // dispatch(connectChain(await getChainState(provider)));
+        // setAccountRedux(provider.getSigner(), dispatch);
+        // store.dispatch(fetchBalances(provider.getSigner()));
 
-        provider.on("block", (blockNumber: number) => {
-            getAndUpdateBlock(dispatch, provider, blockNumber);
-        });
+        // provider.on("block", (blockNumber: number) => {
+        //     getAndUpdateBlock(dispatch, provider, blockNumber);
+        // });
     }
 
     let button = (<></>);
