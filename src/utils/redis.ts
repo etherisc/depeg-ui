644b1a3f24242d75570ff68068e6ff3ba38a8d47
Marc Doerflinger <marc@doerf.li>
2023-08-14 11:59:05 +0000

    add debug log for redis url

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,6 +1,8 @@
 import { createClient } from 'redis';
 
-export const redisClient = createClient({ url: process.env.REDIS_URL || 'redis://redis:6379' });
+const redisUrl = process.env.REDIS_URL || 'redis://redis:6379';
+console.log('Redis URL', redisUrl);
+export const redisClient = createClient({ url: redisUrl });
 redisClient.on('error', err => console.log('Redis Client Error', err));
 redisClient.connect();
 
