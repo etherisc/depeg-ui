bfbb41f7becc00a18ab013a97b2c7fdd0d4cc263
Marc Doerflinger <marc@doerf.li>
2023-12-14 13:47:52 +0000

    remove walletconnect (#964)

--- src/config/walletconnect.ts
+++ src/config/walletconnect.ts
@@ -1,7 +1,7 @@
-import { EthereumClient, w3mConnectors } from "@web3modal/ethereum";
-import { configureChains, createConfig } from "wagmi";
-import { mainnet, polygonMumbai } from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
+// import { EthereumClient, w3mConnectors } from "@web3modal/ethereum";
+// import { configureChains, createConfig } from "wagmi";
+// import { mainnet, polygonMumbai } from 'wagmi/chains';
+// import { publicProvider } from 'wagmi/providers/public';
 
 export const CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID;
 console.log("wallet connect config", "chainid", CHAIN_ID);
@@ -9,26 +9,28 @@ console.log("wallet connect config", "chainid", CHAIN_ID);
 export const WALLET_CONNECT_PROJECT_ID = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "";
 
 // configure walletconnect v2
-let chainsList;
+// let chainsList = [];
 if (CHAIN_ID === "80001") {
     // console.log("wallet connect config", "chainid", chainId, "polygonMumbai");
-    chainsList = [polygonMumbai];
+    // chainsList = [polygonMumbai];
 } else if (CHAIN_ID === "1") {
     // console.log("wallet connect config", "chainid", chainId, "mainnet");
-    chainsList = [mainnet];
+    // chainsList = [mainnet];
 } else {
     // console.log("wallet connect config", "chainid", chainId, "mainnet");
-    chainsList = [mainnet];
+    // chainsList = [mainnet];
 }
 
 // console.log("wallet connect config", "chainsList", chainsList);
 // @ts-ignore testnet not set on mainnet chain - ignore complaint
-const { chains, publicClient } = configureChains(chainsList, [publicProvider()]);
-// console.log("wallet connect config", "chains", chains, "publicClient", publicClient);
-export const wagmiConfig = createConfig({
-    autoConnect: true,
-    connectors: w3mConnectors({ projectId: WALLET_CONNECT_PROJECT_ID, chains }),
-    publicClient
-});
-// console.log("wallet connect config", "wagmiConfig", wagmiConfig);
-export const ethereumClient = new EthereumClient(wagmiConfig, chains);
+// const { chains, publicClient } = configureChains(chainsList, [publicProvider()]);
+// // console.log("wallet connect config", "chains", chains, "publicClient", publicClient);
+// export const wagmiConfig = createConfig({
+//     autoConnect: true,
+//     connectors: w3mConnectors({ projectId: WALLET_CONNECT_PROJECT_ID, chains }),
+//     publicClient
+// });
+// // console.log("wallet connect config", "wagmiConfig", wagmiConfig);
+// export const ethereumClient = new EthereumClient(wagmiConfig, chains);
+export const wagmiConfig = undefined;
+export const ethereumClient = undefined;
--- src/utils/walletconnect.ts
+++ src/utils/walletconnect.ts
@@ -1,21 +1,22 @@
-import { type WalletClient, getWalletClient } from '@wagmi/core';
+// import { type WalletClient, getWalletClient } from '@wagmi/core';
 import { providers } from 'ethers';
 
-export function walletClientToSigner(walletClient: WalletClient) {
-    const { account, chain, transport } = walletClient
-    const network = {
-        chainId: chain.id,
-        name: chain.name,
-        ensAddress: chain.contracts?.ensRegistry?.address,
-    }
-    const provider = new providers.Web3Provider(transport, network)
-    const signer = provider.getSigner(account.address)
-    return signer
+export function walletClientToSigner(walletClient: unknown) {
+    // const { account, chain, transport } = walletClient
+    // const network = {
+    //     chainId: chain.id,
+    //     name: chain.name,
+    //     ensAddress: chain.contracts?.ensRegistry?.address,
+    // }
+    // const provider = new providers.Web3Provider(transport, network)
+    // const signer = provider.getSigner(account.address)
+    return undefined;
 }
 
 /** Action to convert a viem Wallet Client to an ethers.js Signer. */
 export async function getEthersSigner({ chainId }: { chainId?: number } = {}) {
-    const walletClient = await getWalletClient({ chainId })
-    if (!walletClient) return undefined
-    return walletClientToSigner(walletClient)
+    // const walletClient = await getWalletClient({ chainId })
+    // if (!walletClient) return undefined
+    // return walletClientToSigner(walletClient)
+    return undefined;
 }
